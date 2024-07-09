aa19c145ac450530bc2d60b2dda3b3e9be930e1e
AntonPhk <phk.anton@gmail.com>
2024-07-09 11:16:35 +0300

    Minor fixes according PR.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # update db according last alembic commit
-alembic upgrade head
+alembic -c alembic.ini upgrade head
 
 # Start FastAPI app
-uvicorn src.main:app --host 0.0.0.0 --port 8000
+uvicorn src.main:app --host 0.0.0.0 --port 8001
